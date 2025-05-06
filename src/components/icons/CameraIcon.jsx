5f342b7ad15426f58eeeb552b4f21a7d19d4882b
Dev <mondaldev75@gmail.com>
2025-05-07 03:56:26 +0530

    Add CameraIcon JSX component

---
+++ src/components/icons/CameraIcon.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import CustomIcon from './CustomIcon';
+
+const CameraIcon = ({ size = 24, className }) => {
+  return (
+    <CustomIcon size={size} className={className}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z" />
+        <circle cx="12" cy="13" r="3" />
+      </svg>
+    </CustomIcon>
+  );
+};
+
+export default CameraIcon;
